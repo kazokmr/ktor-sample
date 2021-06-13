878b852ba95a8d9de00e310346d3948841d63912
Kazuhiko Okumura <engkokmr@gmail.com>
2021-06-13 20:18:07 +0900

    JSONでPOSTリクエストを送信(登録処理)

--- src/main/kotlin/com/example/routing/BookRoute.kt
+++ src/main/kotlin/com/example/routing/BookRoute.kt
@@ -2,6 +2,7 @@ package com.example.routing
 
 import io.ktor.application.*
 import io.ktor.locations.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -14,7 +15,15 @@ fun Routing.bookRoute() {
             val response = BookResponse(request.bookId, "Kotlin入門", "Kotlin太郎")
             call.respond(response)
         }
+
+        post("/register") {
+            val request = call.receive<RegisterRequest>()
+            // TODO 登録処理
+            call.respondText("registered. id=${request.id} title=${request.title} author=${request.author}")
+        }
     }
 }
 
 data class BookResponse(val id: Long, val title: String, val author: String) {}
+
+data class RegisterRequest(val id: Long, val title: String, val author: String) {}
