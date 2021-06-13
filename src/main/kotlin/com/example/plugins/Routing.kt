888005831bb310ec08d99a761f3e86b5d2889ca6
Kazuhiko Okumura <engkokmr@gmail.com>
2021-06-13 14:07:14 +0900

    リクエストパラメータの追加

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,22 @@
 package com.example.plugins
 
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
 
 fun Application.configureRouting() {
     // Starting point for a Ktor app:
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/hello/{name}") {
+            val name = call.parameters["name"]
+            call.respondText("Hello $name!")
+        }
+        get("/hello") {
+            val name = call.parameters["name"]
+            call.respondText("Hello $name!")
         }
     }
 
